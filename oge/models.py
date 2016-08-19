a40595d52f3734060d448539b2895daecac08105
Olga <korotkaya.olga@yandex.ru>
2016-08-19 12:30:56 +0300

    Change oge.models

--- oge/models.py
+++ oge/models.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 from __future__ import unicode_literals
-
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Tests18_26(models.Model):
@@ -11,3 +10,8 @@ class Tests18_26(models.Model):
 	def __unicode__(self):
 
 		return u'tests18_26 №'+str(self.id)
+
+class Answer(models.Model):
+	test = models.ForeignKey(Tests18_26, on_delete=models.CASCADE)
+	user = models.ForeignKey(User,)
+	answer = models.CharField()
