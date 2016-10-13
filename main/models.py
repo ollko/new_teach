0adafcb012af2fdd578a699e89a88d85a4b126ad
Olga <korotkaya.olga@yandex.ru>
2016-10-13 18:50:39 +0300

    Pillow

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 import datetime
-from PIL import Image
+from Pillow import Image
 import os.path
 from new_teach.settings import BASE_DIR
 from django.core.files import File
