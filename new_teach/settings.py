dd7ad2c9400d7bebe13818a8bcd3202a381aa852
Olga <korotkaya.olga@yandex.ru>
2016-10-19 20:13:42 +0300

    Add STATIC_ROOT

--- new_teach/settings.py
+++ new_teach/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+print 'STATIC_ROOT=',STATIC_ROOT
 # for files uploading:
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
