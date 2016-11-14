a1861b494cc560cd84bf25be70ce5854630db5ad
Olga <korotkaya.olga@yandex.ru>
2016-11-14 14:13:55 +0300

    Remove BASE_DIR from settings.py to local settings.py

--- new_teach/settings.py
+++ new_teach/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from . import DATABASES,SITE_ID
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .local_settings import DATABASES, SITE_ID, BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
